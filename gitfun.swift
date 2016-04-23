7f7a13e1c0d9e8f1ea816033e8ba607f8971aed7
Aaron Eberline <arewwjd@Aarons-MacBook-Pro.local>
2016-04-23 16:08:04 -0500

    Added a function

--- gitfun.swift
+++ gitfun.swift
@@ -1,3 +1,12 @@
 var myfirstchange = "This is a change"
 
 var mysecondchange=1.234
+
+/* Find plugin to show repository for Git in terminal
+ 
+ 
+ */
+
+func addTwoNumvers(numA: Int, numB: Int) -> Int {
+    return numA + numB
+}
